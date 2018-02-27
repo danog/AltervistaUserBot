9247b4c51b2205d53c5599002029ca8576e63640
bruninoit <brunino98@hotmail.it>
2018-02-27 20:07:59 +0100

    version 1.0.2
    fixed !off command
    fixed multi login
    other minor fixes

---
+++ functions.php
@@ -0,0 +1,89 @@
+<?php
+
+
+function failLogin()
+{
+  echo "<h1>LOGIN FALLITO</h1>";
+}
+
+function failUpdates()
+{
+  echo "<h1>USERBOT NON AVVIATO. RIAVVIO.</h1>";
+  file_get_contents($_SERVER['SCRIPT_URI']);
+}
+
+function finePagina(){
+  return true;
+}
+
+function endUpdates()
+{
+  file_get_contents($_SERVER['SCRIPT_URI']);
+  echo "Timeout, lanciato nuovo script.";
+}
+
+
+function sm($chatID, $text, $parsemode = 'HTML', $reply = 0) {
+  global $MadelineProto;
+  if ($reply) $MadelineProto->messages->sendMessage(['peer' => $chatID, 'message' => $text, 'parse_mode' => $parsemode, 'reply_to_msg_id' => $reply]);
+  else        $MadelineProto->messages->sendMessage(['peer' => $chatID, 'message' => $text, 'parse_mode' => $parsemode]);
+}
+
+function setProfilo($nome, $cognome = '')
+{
+  global $MadelineProto;
+  $MadelineProto->account->updateProfile(['first_name' => $nome, 'last_name' => $cognome]);
+}
+
+function joinChat($chatLink, $chatLOG)
+{
+  //ACCETTA SOLO https://t.me/joinchat/ksjdvbdskvhbsdk o @usernameChat in questo formato
+  global $MadelineProto;
+  try{
+    if(stripos($chatLink, "joinchat"))
+    {
+    $MadelineProto->messages->importChatInvite([
+      'hash' => str_replace("https://t.me/joinchat/", "", $chatLink)
+    ]);
+  }else{
+    $MadelineProto->channels->joinChannel([
+      'channel' => "@" . str_replace("@", "", $chatLink)
+    ]);
+  }
+    sm($chatLOG, "Sono entrato nel canale/gruppo");
+  } catch (\danog\MadelineProto\RPCErrorException $e) {
+    sm($chatLOG, "NON sono entrato nel canale/gruppo.");
+  } catch (\danog\MadelineProto\Exception $e2) {
+    sm($chatLOG, "NON sono entrato nel canale/gruppo.");
+  }
+}
+
+function abbandonaChat($chatID)
+{
+  //USARE SOLO SU SUPERGRUPPI o CRASH
+  global $MadelineProto;
+  $MadelineProto->channels->leaveChannel(['channel' => $chatID]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ updates.php
@@ -0,0 +1,67 @@
+<?php
+
+file_put_contents("check.log", "ok");
+require '_config.php';
+register_shutdown_function('failUpdates');
+
+$MadelineProto = new \danog\MadelineProto\API("session.madeline");
+$MadelineProto->session = __DIR__.'/session.madeline';
+
+register_shutdown_function('endUpdates');
+echo "<h1>USERBOT PARTITO</h1>";
+
+$running = true;
+$offset = 0;
+$lastser = time();
+
+while($running)
+{
+  $updates = $MadelineProto->get_updates(['offset' => $offset]);
+  foreach($updates as $update)
+  {
+    $offset = $update['update_id'] + 1;
+
+    if (isset($update['update']['message']['out']) && $update['update']['message']['out'] && !$leggi_messaggi_in_uscita) {
+      continue;
+    }
+    $up = $update['update']['_'];
+
+    if($up == 'updateNewMessage' or $up == 'updateNewChannelMessage')
+    {
+
+      if (isset($update['update']['message']['message'])){
+       $msg = $update["update"]["message"]["message"];
+      }
+
+      if (isset($update['update']['message']['to_id']['channel_id'])) {
+      $chatID = $update['update']['message']['to_id']['channel_id'];
+      $chatID = '-100'.$chatID;
+      $type = "supergruppo";
+      }
+
+      if (isset($update['update']['message']['to_id']['chat_id'])) {
+      $chatID = $update['update']['message']['to_id']['chat_id'];
+      $chatID = '-'.$chatID;
+      $type = "gruppo";
+      }
+
+      if (isset($update['update']['message']['from_id'])) $userID = $update['update']['message']['from_id'];
+
+      if (isset($update['update']['message']['to_id']['user_id'])) {
+      $chatID = $update['update']['message']['from_id'];
+      $type = "privato";
+      }
+
+      @include("_comandi.php");
+
+    }
+
+
+    if(isset($msg)) unset($msg);
+    if(isset($chatID)) unset($chatID);
+    if(isset($userID)) unset($userID);
+    if(isset($up)) unset($up);
+
+
+  }
+}
