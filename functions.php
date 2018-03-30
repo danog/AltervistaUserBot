38bd9cbe557f2c7255030fa7162a8c669efc74ff
Daniil Gentili <daniil@daniil.it>
2018-03-30 18:26:08 +0200

    Bugfix

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function closeConnection($message = 'OK!')
     if (php_sapi_name() === 'cli') {
         return;
     }
-    ob_end_clean();
+    @ob_end_clean();
     header('Connection: close');
     ignore_user_abort(true);
     ob_start();
